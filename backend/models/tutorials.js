2d3d01689783eb7791b09e53e816fff04b930da7
jonayoung <young.jon@icloud.com>
2021-03-21 19:10:00 -0400

    flushed out preliminary schema for tutorials and users under the assumption of future authentication with auth0

--- backend/models/tutorials.js
+++ backend/models/tutorials.js
@@ -1,7 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const tutorialSchema = new Schema({});
+const tutorialSchema = new Schema({
+    tut_id: {
+        type: mongoose.ObjectId,
+        required: true
+    },
+    author: {
+        type: mongoose.ObjectId,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: Number,
+        required: true
+    },
+    time_low: {
+        type: String,
+        required: true
+    },
+    time_high: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    steps: {
+        type: [String],
+        required: true
+    }
+}, { timestamps: true });
 
 const Tutorials = mongoose.model('Tutorial', tutorialSchema);
 module.exports = Tutorials;
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,9 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
-//TODO - The User model will have to be linked to an authentication provider Auth0? How can I link the Auth0
-//TODO - accounts to the User model to pull information (name, birthdate, followed tutorials, etc)?
+const userSchema = new Schema({
+    user_id: {
+        type: mongoose.ObjectId,
+        required: true
+    },
+    intro: {
+        type: String,
+        required: true
+    },
+    created_tuts: {
+        type: [mongoose.ObjectId],
+        required: true
+    },
+    followed_tuts: {
+        type: [mongoose.ObjectId],
+        required: true
+    },
+    category: {
+        type:[String],
+        required: true
+    }
+});
+/*
+TODO - The User model will have to be linked to the auth0 user profile.  I can provied a user_id mongoose.ObjectId
+TODO - to submit to the auth0 user profile.
+
+TODO - On MongoDB can I test the schema values and are correct and that I can successfully store entries before
+TODO - I actually setup the api endpoints?
+*/
 
 
 const Users = mongoose.model('User', userSchema);
